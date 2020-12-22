9918e28db9cd130172a2b57cad50cdf4470ab6ff
Victoria Smith <victoriasmithh428@gmail.com>
2020-12-22 17:09:10 -0500

    created routes and mongoose schema"

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,73 +1,34 @@
 const Router = require("express").Router;
 const router = Router();
+const Workout = require("../model/Workout");
 
-router.get("/workouts", (req, res) => {
-  res.json([
-    {
-      day: new Date().setDate(new Date().getDate() - 10),
-      exercises: [
-        {
-          type: "resistance",
-          name: "Bicep Curl",
-          duration: 20,
-          weight: 100,
-          reps: 10,
-          sets: 4,
-        },
-      ],
-    },
-  ]);
+router.get("/workouts", async (req, res) => {
+  const workouts = await Workout.find();
+  res.json(workouts);
 });
 
-router.put("/workouts/:id", (req, res) => {
-  res.json({
-    _id: req.params.id,
-    day: new Date().setDate(new Date().getDate() - 10),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Bicep Curl",
-        duration: 20,
-        weight: 100,
-        reps: 10,
-        sets: 4,
-      },
-    ],
+router.put("/workouts/:id", async (req, res) => {
+  const workout = await Workout.findById(req.params.id);
+  if (!workout) {
+    return res.status(404).json({ message: "workout doesn't exist" });
+  }
+  const newWorkout = await workout.update({
+    exercises: [...workout.exercises, req.body],
   });
+  res.json(newWorkout);
 });
 
-router.post("/workouts", (req, res) => {
-  res.json({
-    day: new Date().setDate(new Date().getDate() - 10),
-    exercises: [
-      {
-        type: "resistance",
-        name: "Bicep Curl",
-        duration: 20,
-        weight: 100,
-        reps: 10,
-        sets: 4,
-      },
-    ],
+router.post("/workouts", async (req, res) => {
+  const newWorkout = await Workout.create({
+    day: new Date(),
+    exercises: [],
   });
+  res.json(newWorkout);
 });
 
-router.get("/workouts/range", (req, res) => {
-  res.json([
-    {
-      day: new Date().setDate(new Date().getDate() - 10),
-      exercises: [
-        {
-          type: "resistance",
-          name: "Bicep Curl",
-          duration: 20,
-          weight: 100,
-          reps: 10,
-          sets: 4,
-        },
-      ],
-    },
-  ]);
+router.get("/workouts/range", async (req, res) => {
+  const workouts = await Workout.find();
+  res.json(workouts);
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,18 +1,32 @@
 const express = require("express");
 const morgan = require("morgan");
 const router = require("./routes");
+const mongoose = require("mongoose");
 
-const app = express();
+const init = async () => {
+  try {
+    await mongoose.connect(
+      process.env.MONGODB_URI || "mongodb://localhost:27017/workout",
+      { useNewUrlParser: true }
+    );
 
-app.use(morgan("combined"));
-app.use(express.json());
+    const app = express();
 
-app.use(express.static("public"));
-app.use("/stats", express.static("./public/stats.html"));
-app.use("/exercise", express.static("./public/exercise.html"));
+    app.use(morgan("combined"));
+    app.use(express.json());
 
-app.use("/api", router);
+    app.use(express.static("public"));
+    app.use("/stats", express.static("./public/stats.html"));
+    app.use("/exercise", express.static("./public/exercise.html"));
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Listening on ${process.env.PORT || 3000}`);
-});
+    app.use("/api", router);
+
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Listening on ${process.env.PORT || 3000}`);
+    });
+  } catch (e) {
+    console.log("Something Broke", e);
+  }
+};
+
+init();
